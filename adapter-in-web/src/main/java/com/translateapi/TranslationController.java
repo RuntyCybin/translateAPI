21877614d19fa0f3ac7db112628d355192492037
Nikolay Shelevoy <runtycybin@gmail.com>
2026-07-12 13:18:27 +0300

    project infra fixes

--- adapter-in-web/src/main/java/com/translateapi/TranslationController.java
+++ adapter-in-web/src/main/java/com/translateapi/TranslationController.java
@@ -1,5 +1,8 @@
 package com.translateapi;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api/accounts")
 public class TranslationController {
